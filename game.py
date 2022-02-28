d89bbf3050c3c372c47c39f398f02cfb7e820d21
cheethirala varun <92153209+cheethirala-varun@users.noreply.github.com>
2022-02-28 13:51:01 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+import random
+correct = random.randint(1, 99)
+guess = input("Enter your guess: ")
+guess = int(guess)
+while guess != correct:
+	if guess > correct:
+		print("You've guessed too high. Try guessing lower.")
+	
+	else:
+		print("You've guessed too low. Try guessing higher.")
+	guess = int(input("Enter your guess: "))
+print("Congratulations! You've guessed correctly.")
